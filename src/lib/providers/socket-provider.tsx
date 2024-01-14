19817e9aa20b540dc8ff93c587e040e72b3daaa3
PAAPII10 <vishalchaurasia250@gmail.com>
2024-01-15 03:45:23 +0530

    completed project

--- src/lib/providers/socket-provider.tsx
+++ src/lib/providers/socket-provider.tsx
@@ -39,7 +39,7 @@ export const SocketProvider = ({ children }: ISocketProviderProps) => {
         process.env.NEXT_PUBLIC_SITE_URL!,
         {
           path: "/api/socket/io",
-          transports: ["websocket"],
+          transports: ["websocket", "polling"],
           secure: true,
         }
       );
